ee07008a71dd74692aa07ed3b60a2f7818f82aab
xoxics <55588029+xoxics@users.noreply.github.com>
2020-09-06 18:13:06 +0530

    Add files via upload

---
+++ where.js
@@ -0,0 +1,23 @@
+myData = [
+[23.181467,79.9864071, 'Jabalpur, Madhya Pradesh, India'],
+[26.9124336,75.7872709, 'Jaipur, Rajasthan, India'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9503878,77.5022224, 'Mysore Rd, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[10.6779085,78.74454879999999, 'Palkalaiperur, Tiruchirappalli, Tamil Nadu 620024, India'],
+[17.4448019,78.3497604, 'Professor CR Rao Rd, Gachibowli, Hyderabad, Telangana 500032, India'],
+[26.5123388,80.2329, 'Kalyanpur, Kanpur, Uttar Pradesh 208016, India'],
+[19.1334302,72.9132679, 'Main Gate Rd, IIT Area, Powai, Mumbai, Maharashtra 400076, India'],
+[22.3149274,87.31053109999999, 'Kharagpur, West Bengal 721302, India'],
+[23.8143819,86.44120219999999, 'Police Line Road, Main Campus IIT (ISM, IIT (ISM, Hirapur, Sardar Patel Nagar, Dhanbad, Jharkhand 826004, India'],
+[39.1754487,-86.512627, '107 S Indiana Ave, Bloomington, IN 47405, USA'],
+[22.4972777,88.37142779999999, '188, Raja Subodh Chandra Mallick Rd, Jadavpur University Campus Area, Jadavpur, Kolkata, West Bengal 700032, India'],
+[17.4932682,78.3913929, 'Ashok Nagar, Kukatpally Housing Board Colony, Kukatpally, Hyderabad, Telangana 500085, India'],
+[28.5402232,77.1662154, 'New Mehrauli Road, JNU Ring Rd, New Delhi, Delhi 110067, India'],
+[13.0660293,80.28317190000001, 'Navalar Nagar, Chepauk, Triplicane, Chennai, Tamil Nadu 600005, India'],
+[22.2533781,84.9012363, 'jindal colony, Bisra Rd, National Institute of Technology, Udit Nagar, Rourkela, Odisha 769001, India'],
+[21.1468555,79.050062, 'Amravati Rd, Ram Nagar, Nagpur, Maharashtra 440033, India'],
+[1.3483099,103.6831347, '50 Nanyang Ave, Singapore 639798'],
+[31.395929,75.535856, 'Grand Trunk Road, Barnala - Amritsar Bypass Rd, Jalandhar, Punjab 144011, India'],
+[25.0173405,121.5397518, 'No. 1, Section 4, Roosevelt Rd, Da’an District, Taipei City, Taiwan 10617']
+];
